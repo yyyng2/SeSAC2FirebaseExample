3a658209a574c5e8fc2abc7eb5e040ad08a1695c
yyyng2 <yyyng2@gmail.com>
2022-10-05 10:29:31 +0900

    Initial Commit

---
+++ SeSAC2FirebaseExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC2FirebaseExample
+//
+//  Created by Y on 2022/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
